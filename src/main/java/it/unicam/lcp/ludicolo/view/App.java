9b1452feefe8db5376016a877f8953be816a1e36
Zamponi Marco <zamponi.marco3@gmail.com>
2021-05-12 09:37:10 +0200

    [UPDATE] Added new GUI and implemented values refreshing

--- src/main/java/it/unicam/lcp/ludicolo/view/App.java
+++ src/main/java/it/unicam/lcp/ludicolo/view/App.java
@@ -12,11 +12,16 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
@@ -30,42 +35,32 @@ import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class App extends Application {
 
-    public static StackPane pokemonOnePane;
-    public static StackPane pokemonTwoPane;
-    public static TextArea log;
     public static Battle battle;
+    public static Scene scene;
+
+    private static String usedImageOne = "";
+    private static String usedImageTwo = "";
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws InterruptedException {
+    public void start(Stage primaryStage) throws IOException {
 
         primaryStage.setTitle("Ludicolo");
-        StackPane root = new StackPane();
-        primaryStage.setScene(new Scene(root, 800, 600));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(App.class.getResource("/gui.fxml")));
+        scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
-
-        pokemonOnePane = new StackPane();
-        pokemonOnePane.setAlignment(Pos.BOTTOM_CENTER);
-        pokemonTwoPane = new StackPane();
-        pokemonTwoPane.setAlignment(Pos.TOP_CENTER);
-        StackPane logPane = new StackPane();
-        logPane.setAlignment(Pos.CENTER);
-        log = new TextArea();
-        log.setText("This is the start of the log");
-        log.setEditable(false);
-        log.setMaxWidth(400);
-        log.setMaxHeight(200);
-        logPane.getChildren().add(log);
-
-        root.getChildren().addAll(logPane, pokemonOnePane, pokemonTwoPane);
 
         new Thread(() -> {
             KieServices ks = KieServices.Factory.get();
@@ -74,7 +69,7 @@ public class App extends Application {
 
             Map<Item, Integer> playerOneBackpack = new HashMap<>();
             Map<Item, Integer> playerTwoBackpack = new HashMap<>();
-            for (Item singleItem: Item.values()) {
+            for (Item singleItem : Item.values()) {
                 playerOneBackpack.put(singleItem, 5);
                 playerTwoBackpack.put(singleItem, 5);
             }
@@ -94,23 +89,33 @@ public class App extends Application {
     public static void refresh(Pokemon pokemonA, Pokemon pokemonB) {
         Platform.runLater(() -> {
             Pokemon pokemonOne = pokemonA.getOwner().equals(battle.getPlayerOne()) ? pokemonA : pokemonB;
-            Pokemon pokemonTwo = pokemonB.getOwner().equals(battle.getPlayerTwo()) ? pokemonB : pokemonA;
-
-            if (pokemonOnePane.getChildren().size() == 0) {
-                pokemonOnePane.getChildren().add(0, new Text(pokemonOne.getName() + ": " + pokemonOne.getStageValue(Stat.LIFE)));
-            } else {
-                pokemonOnePane.getChildren().set(0, new Text(pokemonOne.getName() + ": " + pokemonOne.getStageValue(Stat.LIFE)));
+            ((Text) scene.lookup("#pokemon_one_name")).setText(pokemonOne.getName() + "\nLv. " + pokemonOne.getLevel());
+            int pokemonOneCurrentHealth = pokemonOne.getStageValue(Stat.LIFE);
+            int pokemonOneMaxHealth = pokemonOne.getBattleStatValue(Stat.LIFE);
+            ((ProgressBar) scene.lookup("#pokemon_one_bar")).setProgress((double) pokemonOneCurrentHealth / pokemonOneMaxHealth);
+            ((Text) scene.lookup("#pokemon_one_life")).setText(pokemonOneCurrentHealth + "/" + pokemonOneMaxHealth);
+            String newImageOne = App.class.getResource("/assets/" + pokemonOne.getName().toUpperCase() + "_back.gif").toExternalForm();
+            if (!newImageOne.equals(usedImageOne)) {
+                ((ImageView) scene.lookup("#pokemon_one_img")).setImage(new Image(newImageOne));
+                usedImageOne = newImageOne;
             }
-            if (pokemonTwoPane.getChildren().size() == 0) {
-                pokemonTwoPane.getChildren().add(0, new Text(pokemonTwo.getName() + ": " + pokemonTwo.getStageValue(Stat.LIFE)));
-            } else {
-                pokemonTwoPane.getChildren().set(0, new Text(pokemonTwo.getName() + ": " + pokemonTwo.getStageValue(Stat.LIFE)));
+
+            Pokemon pokemonTwo = pokemonB.getOwner().equals(battle.getPlayerTwo()) ? pokemonB : pokemonA;
+            ((Text) scene.lookup("#pokemon_two_name")).setText(pokemonTwo.getName() + "\nLv. " + pokemonTwo.getLevel());
+            int pokemonTwoCurrentHealth = pokemonTwo.getStageValue(Stat.LIFE);
+            int pokemonTwoMaxHealth = pokemonTwo.getBattleStatValue(Stat.LIFE);
+            ((ProgressBar) scene.lookup("#pokemon_two_bar")).setProgress((double) pokemonTwoCurrentHealth / pokemonTwoMaxHealth);
+            ((Text) scene.lookup("#pokemon_two_life")).setText(pokemonTwoCurrentHealth + "/" + pokemonTwoMaxHealth);
+            String newImageTwo = App.class.getResource("/assets/" + pokemonTwo.getName().toUpperCase() + "_front.gif").toExternalForm();
+            if (!newImageTwo.equals(usedImageTwo)) {
+                ((ImageView) scene.lookup("#pokemon_two_img")).setImage(new Image(newImageTwo));
+                usedImageTwo = newImageTwo;
             }
         });
     }
 
     public static void log(String s) {
-        Platform.runLater(() -> log.appendText(System.lineSeparator() + s));
+        Platform.runLater(() -> ((TextArea) scene.lookup("#log_pane")).appendText(System.lineSeparator() + "> " + s));
         System.out.println(s);
     }
 }
